9560009c19d80c465f699672371133d23d787243
baba-beda <darya.nightmare@gmail.com>
2017-05-15 20:45:43 +0300

    added description of loadGEO

--- r.sh
+++ r.sh
@@ -2,7 +2,7 @@
 
 pdflatex bachelor-thesis
 biber 	 bachelor-thesis
-dflatex bachelor-thesis
+pdflatex bachelor-thesis
 pdflatex bachelor-thesis
 
 rm bachelor-thesis.{aux,log,bbl,bcf,blg,run.xml,toc,tct}
@@ -11,5 +11,3 @@ pdftk bachelor-thesis.pdf cat 1-2 output build/title.pdf
 pdftk bachelor-thesis.pdf cat 3-4 output build/specification.pdf
 pdftk bachelor-thesis.pdf cat 5-6 output build/annotation.pdf
 pdftk bachelor-thesis.pdf cat 7-end output build/thesis.pdf
-
-rm bachelor-thesis.pdf
