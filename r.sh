798529c03721b36f7aaf8378a0e138e063a05ff4
baba-beda <darya.nightmare@gmail.com>
2017-05-15 13:14:51 +0300

    splitted into several docs

--- r.sh
+++ r.sh
@@ -6,3 +6,10 @@ dflatex bachelor-thesis
 pdflatex bachelor-thesis
 
 rm bachelor-thesis.{aux,log,bbl,bcf,blg,run.xml,toc,tct}
+
+pdftk bachelor-thesis.pdf cat 1-2 output build/title.pdf
+pdftk bachelor-thesis.pdf cat 3-4 output build/specification.pdf
+pdftk bachelor-thesis.pdf cat 5-6 output build/annotation.pdf
+pdftk bachelor-thesis.pdf cat 7-end output build/thesis.pdf
+
+rm bachelor-thesis.pdf
