a9e15c496ba9b74847e8a485e6cde405c98c12e0
baba-beda <darya.nightmare@gmail.com>
2017-06-07 03:06:11 +0300

    finished (?) with third chapter + written conclusion

--- r.sh
+++ r.sh
@@ -8,7 +8,7 @@ pdflatex -halt-on-error bachelor-thesis
 rm bachelor-thesis.{aux,log,bbl,bcf,blg,run.xml,toc,tct}
 
 pdftk bachelor-thesis.pdf cat 1-2 output build/title-page.pdf
-#pdftk bachelor-thesis.pdf cat 3-4 output build/specification.pdf
-#pdftk bachelor-thesis.pdf cat 5-6 output build/annotation.pdf
-pdftk bachelor-thesis.pdf cat 3-end output build/thesis.pdf
+pdftk bachelor-thesis.pdf cat 3-4 output build/specification.pdf
+pdftk bachelor-thesis.pdf cat 5-6 output build/annotation.pdf
+pdftk bachelor-thesis.pdf cat 7-end output build/thesis.pdf
 
